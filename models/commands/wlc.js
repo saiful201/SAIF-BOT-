3c5c6c39f9856068370dc3f70b82d6b3fe7dc2c3
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2025-02-09 22:10:57 +0530

    Update wlc.js

--- models/commands/wlc.js
+++ models/commands/wlc.js
@@ -1,33 +1,40 @@
 const fs = require("fs");
+const path = require("path"); // Import the path module
+
 module.exports.config = {
-	name: "priyansh",
+  name: "admin2",
   version: "1.0.1",
-	hasPermssion: 0,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
-	description: "hihihihi",
-	commandCategory: "no prefix",
-	usages: "priyansh",
-    cooldowns: 5, 
+  hasPermssion: 0,
+  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "admin",
+  cooldowns: 5,
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) 
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  const { threadID, messageID } = event;
+  const triggerWords = ["WELCOME", "Welcome", "Wlc", "welcome"]; // Array for easier checking
 
-  { 
-    if (this.config.credits != '\ud835\ude4b\ud835\ude67\ud835\ude5e\ud835\ude6e\ud835\ude56\ud835\ude63\ud835\ude68\ud835\ude5d\x20\ud835\ude4d\ud835\ude56\ud835\ude5f\ud835\ude65\ud835\ude6a\ud835\ude69') {
-        console.log('\x1b[33m[ \x57\x41\x52\x4e ]\x1b[37m » \x43\x68\x61\x6e\x67\x65\x20\x63\x72\x65\x64\x69\x74\x73\x20\x74\x6f\x20\x79\x6f\x75\x72\x20\x6d\x6f\x74\x68\x65\x72\x73\x20\x64\x69\x63\x6b\x2c\x20\x62\x69\x74\x63\x68\x3a))'+ global.config.BOTNAME + ' \x63\x68\x61\x6e\x67\x65\x20\x63\x72\x65\x64\x69\x74\x73\x20\x6d\x6f\x64\x75\x6c\x65\x73 "' + this.config.name + '"');
-        return api.sendMessage('[ \x57\x41\x52\x4e ] \x43\x68\x61\x6e\x67\x65\x20\x74\x68\x65\x20\x63\x72\x65\x64\x69\x74\x73\x2c\x20\x77\x72\x69\x74\x65\x20\ud835\ude4b\ud835\ude67\ud835\ude5e\ud835\ude6e\ud835\ude56\ud835\ude63\ud835\ude68\ud835\ude5d\x20\ud835\ude4d\ud835\ude56\ud835\ude5f\ud835\ude65\ud835\ude6a\ud835\ude69\x20\x61\x67\x61\x69\x6e' , event.threadID, event.messageID);
-    }
+  if (triggerWords.some(word => event.body.indexOf(word) === 0)) {
+    const imagePath = path.join(__dirname, "noprefix", "wlc.jpeg"); // Use path.join for cross-platform compatibility
 
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("WELCOME")==0 || event.body.indexOf("wlc")==0 || event.body.indexOf("welcome")==0 || event.body.indexOf("Welcome")==0) {
-		var msg = {
-				body: "WELCOME ✨💖🥀",
-				attachment: fs.createReadStream(__dirname + `/noprefix/wlc.png`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
-
+    // Check if the file exists *before* trying to read it
+    fs.access(imagePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.error("Error: wlc.jpeg not found!", err);
+        api.sendMessage("Welcome to the group!", threadID, messageID); // Fallback message
+      } else {
+        const msg = {
+          body: "😻 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏 😻",
+          attachment: fs.createReadStream(imagePath),
+        };
+        api.sendMessage(msg, threadID, messageID);
+        api.setMessageReaction("🙈", messageID, (err) => {}, true); // Use messageID directly
+      }
+    });
   }
+};
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {};
+

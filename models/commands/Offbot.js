1172be056ef8284bfb0bf66e0461dc86267b6eb5
saiful201 <118247523+saiful201@users.noreply.github.com>
2025-02-22 19:03:43 +0300

    Update Offbot.js

--- models/commands/Offbot.js
+++ models/commands/Offbot.js
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100017784376294", "100037743553265"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Saurabh", event.threadID, event.messageID);
+    const permission = ["100011152309303", "100011152309303"];
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Saiful islam", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
